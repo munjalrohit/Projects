5fb3f563beceffd65bac7aad7aaa561bb855246d
munjalrohit <munjalrohit@gmail.com>
2013-03-12 17:00:54 +0530

    Commit message By Rohit Munjal

---
+++ App_Code/Testwebservice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for Testwebservice
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class Testwebservice : System.Web.Services.WebService {
+
+    public Testwebservice () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hello World";
+    }
+
+
+    [WebMethod]
+    public string Login(String Username, String password, Int32 ProductID)
+    {
+        return "<Message>You have supplied an incorrect password</Message><Code/><Succcess>false</Succcess><Result/>";
+
+    }
+    
+
+}
